8d29173937ccbfa8acb88070e1fe415590cf91c3
sumaeta <brenosumaeta33@gmail.com>
2022-06-06 22:49:35 -0300

    Mapeando as entidades com JPA

---
+++ src/main/java/com/dev/eats/DevEatsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.eats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevEatsApplication.class, args);
+	}
+
+}
